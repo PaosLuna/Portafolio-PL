7a3806ec6129d99596205b973f1a61e2b028a616
PaosLuna <lunapp20@gmail.com>
2023-10-04 15:48:30 -0600

    Primer commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,24 @@
+import logo from "../assets/Images/logo.png";
+
+const Header = () => {
+  return (
+    <div className="flex flex-row w-screen bg-black">
+      <div className="w-28 p-2">
+        <img src={logo} alt="Logo Paola Luna" className=" pl-10" />
+      </div>
+      <nav className="w-full flex items-center justify-end">
+        <div>
+          <ul className="flex flex-row gap-10 p-2 pr-40 text-white font-mono uppercase text-xl cursor-pointer">
+            <li>Home</li>
+            <li>About Me</li>
+            <li>Projects</li>
+            <li>Skills</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
