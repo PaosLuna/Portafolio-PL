f7d9dc90255083e3bf831a2fd2ac51587643a334
PaosLuna <lunapp20@gmail.com>
2023-10-05 00:08:09 -0600

    Feat skills

---
+++ src/Data/skillsTech.js
@@ -0,0 +1,58 @@
+export const frontendSkills = [
+  {
+    name: "HTML5",
+    icon: "https://assets.stickpng.com/images/5847f5bdcef1014c0b5e489c.png",
+  },
+  {
+    name: "CSS3",
+    icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+  },
+  {
+    name: "JavaScript (ES6+)",
+    icon: "https://seeklogo.com/images/J/javascript-logo-E967E87D74-seeklogo.com.png",
+  },
+  {
+    name: "React",
+    icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png",
+  },
+  {
+    name: "React Router",
+    icon: "https://reactrouter.com/_brand/react-router-mark-color.png",
+  },
+  {
+    name: "Git",
+    icon: "https://git-scm.com/images/logos/downloads/Git-Icon-1788C.png",
+  },
+  {
+    name: "GitHub",
+    icon: "https://cdn-icons-png.flaticon.com/512/25/25231.png",
+  },
+  {
+    name: "Jest",
+    icon: "https://cdn.freebiesupply.com/logos/large/2x/jest-logo-png-transparent.png",
+  },
+  {
+    name: "Lighthouse",
+    icon: "https://seeklogo.com/images/G/google-lighthouse-logo-F4457EC878-seeklogo.com.png",
+  },
+  {
+    name: "MySql",
+    icon: "https://res.cloudinary.com/hevo/image/upload/f_auto,q_auto/v1685802629/hevo-blog-3/MySQL-Logo.wine_.png?_i=AA",
+  },
+  {
+    name: "Node.js",
+    icon: "https://upload.wikimedia.org/wikipedia/commons/d/d9/Node.js_logo.svg",
+  },
+  {
+    name: "Firebase",
+    icon: "https://seeklogo.com/images/F/firebase-logo-402F407EE0-seeklogo.com.png",
+  },
+  {
+    name: "Figma",
+    icon: "https://brandlogos.net/wp-content/uploads/2022/05/figma-logo_brandlogos.net_6n1pb.png",
+  },
+  {
+    name: "Scrum",
+    icon: "https://pluralsight2.imgix.net/paths/images/scrum-a5c44d8364.png",
+  },
+];
--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,7 +1,34 @@
+import { frontendSkills } from "../Data/skillsTech";
+import Header from "../components/Header";
+
 const Skills = () => {
   return (
-    <div>
-      <h1>SKILLS</h1>
+    <div className="flex flex-col bg-black h-screen w-screen">
+      <Header />
+      <div className="flex justify-center mt-10">
+        <div className="flex flex-col justify-center items-center bg-[#F6229B] w-[40%] mt-5">
+          <h1 className="text-black uppercase text-3xl font-mono font-extrabold pt-5">
+            Tech skills
+          </h1>
+          <div className="flex flex-wrap gap-4 justify-center p-5">
+            {frontendSkills.map((skill, index) => (
+              <div
+                key={index}
+                className="p-2 border border-black bg-black rounded-md w-40 h-20 flex items-center gap-2 justify-center"
+              >
+                <img
+                  src={skill.icon}
+                  alt={`${skill.name} icon`}
+                  className="w-7 h-7"
+                />
+                <span className="text-lg font-mono font-bold text-white">
+                  {skill.name}
+                </span>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
