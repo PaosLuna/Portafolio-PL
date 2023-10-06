7a90482b7397d10a80422b50be41bd830dc7bca5
PaosLuna <lunapp20@gmail.com>
2023-10-05 21:23:35 -0600

    Feat responsive AboutMe

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -5,15 +5,23 @@ import contexto from "../assets/Images/contexto.png";
 const AboutMe = () => {
   return (
     <div>
-      <div className="flex flex-col min-h-screen bg-black max-w-screen">
+      <div className="bg-black min-h-screen max-w-screen">
         <Header />
-        <div className="flex p-10">
-          <div className="w-[40%] p-2 flex justify-center">
-            <img src={fotoCard} alt="foto" className="w-[340px] h-auto" />
+        <div className="p-3 flex flex-col items-center lg:flex lg:flex-row lg:p-10">
+          <div className="p-2 w-[100%] lg:w-[40%] flex justify-center">
+            <img
+              src={fotoCard}
+              alt="foto"
+              className="w-[240px] md:w-[300px] lg:w-[340px] h-auto"
+            />
           </div>
-          <div className="w-[65%] flex flex-col justify-center items-center">
-            <img src={contexto} alt="titulo" className="w-[500px]" />
-            <p className="text-white font-mono text-xl text-justify mt-5 w-[90%] p-10">
+          <div className="flex flex-col justify-center items-center w-[90%] mt-5 md:mt-10 lg:w-[65%]">
+            <img
+              src={contexto}
+              alt="titulo"
+              className="w-[280px] md:w-[400px] lg:w-[500px]"
+            />
+            <p className="text-white font-mono text-xl text-justify p-5 md:mt-8 lg:mt-5 lg:w-[90%] lg:p-10">
               👩‍💻 Iniciando en el mundo IT, emocionada por aprender y mejorar
               cada día. <br />
               <br /> 🗣️ Con un background en Comunicación, me encanta encontrar
